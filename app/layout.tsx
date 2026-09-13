5682b885088e05f0dc38d6bd3a1b3a8dbdeb909f
Magnufinuf <5r6675nz9h@privaterelay.appleid.com>
2026-09-13 23:21:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Sidebar from "./components/Sidebar";
+import ThemeInit from "./components/ThemeInit";
 
 export const metadata: Metadata = {
   title: "Shopsearch",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="no" className="h-full antialiased">
       <body className="min-h-full flex">
+        <ThemeInit />
         <Sidebar />
         <div style={{ flex: 1 }}>{children}</div>
       </body>
